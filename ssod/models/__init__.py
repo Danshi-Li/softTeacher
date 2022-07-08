fccd103adda296302560258482d4e5bfcb6f5279
Danshi-Li <1155107681@link.cuhk.edu.hk>
2022-07-08 19:13:12 +0800

    see if this works

--- ssod/models/__init__.py
+++ ssod/models/__init__.py
@@ -1,3 +1,4 @@
 from .soft_teacher import SoftTeacherGradCAM
 from .soft_teacher_backup import SoftTeacher
 from .clip_rpn.CLIP_rpn import CLIPRPNHead
+from .clip_gradcam.rpn_withcam import RPNHeadWithCAM
